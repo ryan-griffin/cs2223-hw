d79ee059c7d1ac2224efb5f0a63701fbf129ad38
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2024-03-28 14:56:08 -0400

    add ryan sequence

--- Griffin-Ryan-D24-HW2/Main.java
+++ Griffin-Ryan-D24-HW2/Main.java
@@ -1,43 +1,42 @@
 public class Main {
 	public static void main(String[] args) {
-		if (args.length != 1) {
-			System.out.println("Usage: java Main <n>");
+		if (args.length != 2) {
+			System.out.println("Usage: java Main <sequence> <n>");
 			System.exit(1);
 		}
 
-		int n = Integer.parseInt(args[0]);
+		String sequence = args[0].trim().toLowerCase();
+		int n = Integer.parseInt(args[1]);
 
-		System.out.println("Lucas numbers up to " + n + ":");
+		System.out.println(sequence + " numbers up to " + n + ":");
 
-		int prevLucas = 0;
+		int prevNum = 0;
 		long prevTime = 0;
 		for (int i = 0; i <= n; i++) {
 			long startTime = System.nanoTime();
-			int lucasNumber = lucas(i);
+			int num = sequence.equals("lucas") ? lucas(i) : ryan(i);
 			long endTime = System.nanoTime();
 			long elapsedTime = endTime - startTime;
 
-			System.out.println("Lucas(" + i + "): " + lucasNumber + ", Time: " + elapsedTime + "ns");
+			System.out.println("Lucas(" + i + "): " + num + ", Time: " + elapsedTime + "ns");
 
 			if (i > 1) {
-				double ratio = (double) lucasNumber / prevLucas;
+				double ratio = (double) num / prevNum;
 				double timeRatio = (double) elapsedTime / prevTime;
 
 				System.out.println("Ratio: " + ratio + ", Time Ratio: " + timeRatio + "\n");
 			}
 
-			prevLucas = lucasNumber;
+			prevNum = num;
 			prevTime = elapsedTime;
 		}
 	}
 
 	private static int lucas(int n) {
-		if (n == 0) {
-			return 2;
-		}
-		if (n == 1) {
-			return 1;
-		}
-		return lucas(n - 1) + lucas(n - 2);
+		return n == 0 ? 2 : n == 1 ? 1 : lucas(n - 1) + lucas(n - 2);
+	}
+
+	private static int ryan(int n) {
+		return n == 0 ? 3 : n == 1 ? 12 : ryan(n - 1) + ryan(n - 1) * 2;
 	}
 }
