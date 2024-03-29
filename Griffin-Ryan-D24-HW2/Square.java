bfbf1c79641f57bca1f4c8e8b05394ebd56503f8
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2024-03-28 20:24:41 -0400

    rework square

--- Griffin-Ryan-D24-HW2/Square.java
+++ Griffin-Ryan-D24-HW2/Square.java
@@ -1,36 +1,23 @@
+import java.util.HashMap;
+
 public class Square {
+	private static int[] square = { 1, 14, 14, 4, 11, 7, 6, 9, 8, 10, 10, 5, 13, 2, 3, 15 };
+	private static int sum = 33;
+
 	public static void main(String[] args) {
-		int[][] magicSquare = {
-				{ 1, 14, 14, 4 },
-				{ 11, 7, 6, 9 },
-				{ 8, 10, 10, 5 },
-				{ 13, 2, 3, 15 }
-		};
-
-		int targetSum = magicSquare[0][0] + magicSquare[1][1] + magicSquare[2][2] + magicSquare[3][3];
-
-		int combinationsCount = countCombinations(magicSquare, targetSum);
-		System.out.println("Number of 4-element combinations with the same sum as rows/columns: " + combinationsCount);
+		System.out.println("Number of 4-element combinations with the same sum: " + countFourElementCombinations());
+		System.out.println("Number of all combinations with the same sum: " + countAllCombinations());
+		System.out.println("Number of ways every possible sum can be formed: " + countWaysForSums());
 	}
 
-	private static boolean hasTargetSum(int[] arr, int targetSum) {
-		int sum = 0;
-		for (int num : arr) {
-			sum += num;
-		}
-		return sum == targetSum;
-	}
-
-	private static int countCombinations(int[][] square, int targetSum) {
+	private static int countFourElementCombinations() {
 		int count = 0;
-		int n = square.length;
-
-		for (int i = 0; i < n - 1; i++) {
-			for (int j = 0; j < n - 1; j++) {
-				for (int k = i + 1; k < n; k++) {
-					for (int l = j + 1; l < n; l++) {
-						int[] combination = { square[i][j], square[i][l], square[k][j], square[k][l] };
-						if (hasTargetSum(combination, targetSum)) {
+		for (int i = 0; i < square.length; i++) {
+			for (int j = i + 1; j < square.length; j++) {
+				for (int k = j + 1; k < square.length; k++) {
+					for (int l = k + 1; l < square.length; l++) {
+						if (square[i] + square[j] + square[k]
+								+ square[l] == sum) {
 							count++;
 						}
 					}
@@ -38,5 +25,31 @@ public class Square {
 			}
 		}
 		return count;
+	}
+
+	private static int countAllCombinations() {
+		int count = 0;
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < square.length; i++) {
+			for (int j = i; j < square.length; j++) {
+				int currentSum = square[i] + square[j];
+				if (map.containsKey(sum - currentSum)) {
+					count += map.get(sum - currentSum);
+				}
+				map.put(currentSum, map.getOrDefault(currentSum, 0) + 1);
+			}
+		}
+		return count;
+	}
+
+	private static int countWaysForSums() {
+		int[] dp = new int[sum + 1];
+		dp[0] = 1;
+		for (int i = 0; i < square.length; i++) {
+			for (int j = sum; j >= square[i]; j--) {
+				dp[j] += dp[j - square[i]];
+			}
+		}
+		return dp[sum];
 	}
 }
