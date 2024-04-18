6bde267108cb247d2dc9a614bcfc6f4ee8165685
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2024-04-17 20:43:35 -0400

    display some information

--- Griffin-Ryan-D24-HW5/HashWords.java
+++ Griffin-Ryan-D24-HW5/HashWords.java
@@ -39,6 +39,18 @@ public class HashWords {
 		}
 
 		displayHashTable(hashTable);
+
+		int emptyCount = countNonEmptyaddresses(hashTable);
+		System.out.println("Number of non-empty addresses: " + emptyCount);
+		System.out.println("Load factor: " + getLoadFactor(hashTable, emptyCount) + "\n");
+
+		int[] longestEmptyArea = getLongestArea(hashTable, true);
+		System.out.println("Longest empty area: "
+				+ longestEmptyArea[0] + " to " + longestEmptyArea[1] + "\n");
+
+		int[] longestNonEmptyArea = getLongestArea(hashTable, false);
+		System.out.println("Longest non-empty area: "
+				+ longestNonEmptyArea[0] + " to " + longestNonEmptyArea[1]);
 	}
 
 	private static int computeHash(String word, int c, int m) {
@@ -56,5 +68,53 @@ public class HashWords {
 				System.out.println(i + ", " + hashTable[i][0] + ", " + hashTable[i][1]);
 			}
 		}
+		System.out.println();
 	}
+
+	private static int countNonEmptyaddresses(String[][] hashTable) {
+		int count = 0;
+		for (int i = 0; i < hashTable.length; i++) {
+			if (hashTable[i][0] != null) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	private static double getLoadFactor(String[][] hashTable, int count) {
+		return (double) count / hashTable.length;
+	}
+
+	private static int[] getLongestArea(String[][] hashTable, boolean empty) {
+		int maxStart = -1;
+		int maxLength = 0;
+		int currentStart = -1;
+		int currentLength = 0;
+
+		for (int i = 0; i < hashTable.length; i++) {
+			boolean condition = empty ? hashTable[i][0] == null : hashTable[i][0] != null;
+
+			if (condition) {
+				if (currentStart == -1) {
+					currentStart = i;
+				}
+				currentLength++;
+			} else {
+				if (currentLength > maxLength) {
+					maxStart = currentStart;
+					maxLength = currentLength;
+				}
+				currentStart = -1;
+				currentLength = 0;
+			}
+		}
+
+		if (currentLength > maxLength) {
+			maxStart = currentStart;
+			maxLength = currentLength;
+		}
+
+		return new int[] { maxStart, maxStart + maxLength - 1 };
+	}
+
 }
