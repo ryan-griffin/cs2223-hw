a2146c7088d09661b474b26214ff249fdfe3caff
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2024-04-27 20:15:02 -0400

    remove param

--- Griffin-Ryan-D24-HW6/NQueens.java
+++ Griffin-Ryan-D24-HW6/NQueens.java
@@ -2,32 +2,31 @@ import java.util.Arrays;
 
 public class NQueens {
 	public static void main(String[] args) {
-		int n = 8;
-
 		int[] board1 = { 1, 6, 8, 3, 5, 0, 0, 0 };
-		printNextLegal(board1, n);
+		printNextLegal(board1);
 
 		int[] board2 = { 1, 6, 8, 3, 7, 0, 0, 0 };
-		printNextLegal(board2, n);
+		printNextLegal(board2);
 
 		int[] board3 = { 1, 6, 8, 3, 7, 4, 2, 5 };
-		printNextLegal(board3, n);
+		printNextLegal(board3);
 	}
 
-	private static void printNextLegal(int[] board, int n) {
+	private static void printNextLegal(int[] board) {
 		System.out.println(
 				"Next legal position from " + Arrays.toString(board) + " is "
-						+ Arrays.toString(nextLegalPosition(board, n)));
+						+ Arrays.toString(nextLegalPosition(board)));
 	}
 
-	private static int[] nextLegalPosition(int[] board, int n) {
+	private static int[] nextLegalPosition(int[] board) {
 		do
-			board = Successor(board, n);
-		while (!isLegalPosition(board, n));
+			board = Successor(board);
+		while (!isLegalPosition(board));
 		return board;
 	}
 
-	private static int[] Successor(int[] board, int n) {
+	private static int[] Successor(int[] board) {
+		int n = board.length;
 		int i = n - 1;
 		while (i >= 0 && board[i] == n)
 			i--;
@@ -41,7 +40,8 @@ public class NQueens {
 		return board;
 	}
 
-	private static boolean isLegalPosition(int[] board, int n) {
+	private static boolean isLegalPosition(int[] board) {
+		int n = board.length;
 		for (int i = 0; i < n; i++) {
 			if (board[i] == 0)
 				continue;
